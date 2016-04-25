49e5c59f46e23cbc27182bdb57f9227ad4d3205d
aczarnecki3 <annieczarnecki@CE029-ATC.local>
2016-04-25 17:35:55 -0400

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,69 @@
+function showMenu() {
+		$("#mobile-menu").toggleClass('slide');
+	}
+
+
+
+
+$(function() {               
+
+$("#hamburger").click(showMenu);
+$("#main").parallax({imageSrc: '../partnerportal/images/hero.png'});
+$("h1").fadeIn(1500);
+
+
+;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+GENERAL:
+
+- The background of the "logout" button will be a light green on hover.
+- #main-buttons will change to dark blue when user hovers. 
+- #main-buttons will also be the same size.
+- There will be a form (potentially progressive) on the 'Submit a Lead' Page.
+- Some of the in-between states are not quite mobile responsive yet. Need to improve here and decide on hero image.
+- Add "Back to Home" navigation link?
+- Add Contact Us or Social Images at bottom of all pages
+
+
+Resource Page:
+
+(A lot of build-out to be done here.)
+
+- Need to create sidebar or aside with list of content types: Case Studies, Sales Tools, Videos, etc.
+- Want to have a filter, where the user selects from that list and the resources that apply populate in resource blocks
+- Animate resource boxes to expand, contract, fade in/out...something fun here.
+
+
+
+
+*/
